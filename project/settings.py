c9ae96647d34685ff4c7aa4f911209b3751137a0
souhaillafhais <128721103+souhaillafhais@users.noreply.github.com>
2024-06-04 02:25:51 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -143,8 +143,8 @@ LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER ='athepro17@gmail.com'
-EMAIL_HOST_PASSWORD ='lrgl rbem reql hqxw'
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_TLS = True
 EMAIL_PORT ='587'
 
